4402c2d336b7f978482d32c0a9d0e16169d32516
idan <72699248+idanbe11@users.noreply.github.com>
2023-04-10 09:07:46 +0300

    commit

--- shopify/src/components/Home.js
+++ shopify/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import banner from'../assets/images/banner.png';
 
 
 const Home = () => {
@@ -7,10 +8,7 @@ const Home = () => {
     return (
         <div id="home" className="section">
             <div className='heroSection'>
-                <h2>כותרת קטנה לכפתור דף בית</h2>
-                <h1>כותרת גדולה לכפתור דף הבית כן כן</h1>
-                <p>פסקה לכפתור דף הבית לורם לורם לרום בואו שבו ילדים אספר לכם סיפור ממש ממש נחמד כן כן מה שאתם שומעים נספר לך סיפור יפה יפה לפני הרבה הרבה הרבה שנים בואו שלושה ילדים ששיחקו מייפל סטורי אותם ילדים שבורים חסרי חיים החילטו שהם רוצים להיות מיליונרים מההההה זההההה כן כן מה שאתם שומעים תאמינו בעצמכם ילדים חמודים אם אתם רוצים להגיע לאנשהו בחיים שלכם אז תגיעו</p>
-                <a href="https://calendly.com/idanbeneliyaho/30min" target="_blank" rel="noopener noreferrer"> <button  className="contact-btn">צור קשר איתנו</button></a>
+            <img src={banner} alt="banner"/>
             </div>
         </div>
       );
